6a33211593310c9e7b296bc774b0eb456a2b68b3
Josef Habib <josef.habib@kcl.ac.uk>
2024-11-16 22:17:43 +0100

    feat: implement route POST /users to create new users

--- core-be/src/users/users.controller.ts
+++ core-be/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  // POST /users: Create a new user
+  @Post()
+  createUser() {
+      // TODO: Implement createUser() controller method
+      return 'This action adds a new user';
+    }
+  
+
+  // GET /users: Get all users
+  // GET /users/:id: Get a user by id
+  // PUT /users/:id: Update a user by id
+  // DELETE /users/:id: Delete a user by id
+
+
+}
