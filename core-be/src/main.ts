ca9a1d5ab4ed91a9742a6fed20146b0cf8c98b25
Josef Habib <josef.habib@kcl.ac.uk>
2024-11-16 17:18:53 +0100

    feat: add TODOs for future enhancements in main.ts (config service, environment port, logging)

--- core-be/src/main.ts
+++ core-be/src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO: Add config service
+// TODO: Use the port from the environment variables
+// TODO: Add logging
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
