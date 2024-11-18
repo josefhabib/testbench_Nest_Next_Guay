a44020eeb39ca388ca26cf600030a85c1c9f5c96
Josef Habib <josef.habib@kcl.ac.uk>
2024-11-18 12:35:32 +0100

    refactor: (minor)

--- core-be/src/main.ts
+++ core-be/src/main.ts
@@ -8,11 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe(    // Globally enable validation pipe
-    {                                       // Optional configuration: Only allow properties that are defined in the DTO
-      whitelist: true,  
-    }
-  )); 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true } )); 
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
