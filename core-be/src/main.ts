f24bf9d432014c1e2632c9478a91709bfc5d8878
Josef Habib <josef.habib@kcl.ac.uk>
2024-11-18 12:31:08 +0100

    feat: implement global validation pipe for request validation (+ manual testing)

--- core-be/src/main.ts
+++ core-be/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 // TODO: Add config service
 // TODO: Use the port from the environment variables
@@ -7,6 +8,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(    // Globally enable validation pipe
+    {                                       // Optional configuration: Only allow properties that are defined in the DTO
+      whitelist: true,  
+    }
+  )); 
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
