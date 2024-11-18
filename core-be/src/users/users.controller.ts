5a52d58d9fee8bd92c1fd0a6ca311e40a3aee837
Josef Habib <josef.habib@kcl.ac.uk>
2024-11-18 10:02:59 +0100

    feat: DI the usersService -> usersController; Implement createUser method in UsersController

--- core-be/src/users/users.controller.ts
+++ core-be/src/users/users.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Post } from '@nestjs/common';
 import { Body } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+  // Constructor (dependency injection)
+  constructor(private readonly usersService: UsersService) {}
+
   // POST /users: Create a new user
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-    // TODO: Implement createUser() controller method
-    return 'This action adds a new user';
+  createUser(@Body() req: CreateUserDto) {
+    return this.usersService.createUser(req);
   }
   
 
