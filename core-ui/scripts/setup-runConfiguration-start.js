de2cd90952784c8464db1f7821923791d7496163
Josef Habib <josef.habib@kcl.ac.uk>
2025-01-22 16:22:40 +0100

    fix/kludge: Add support to read in port from .env files
    This step unexpectedly faced a number of obstacles (I would have assumed
    that setting the port would be a simple task). The main issues were that:
    - the env file was not being loaded
    - when running the script from package.json an old version of node (16.13.2) was being used (system version: 21.1.0)
    
    In the end the solution was to write scripts to load the various run configs
    explicitly. This is not ideal (as I was not able to identify the setup problem
    yet), but it works (and arguably is the cleaner solution anyway).
    
    ToDo:
    - [ ] Investigate why the env file is not being loaded
    - [ ] Investigate why the system node version is not being used
    - [ ] Ensure that the change in .env has not resulted in any issues with sibling components (NestJS, Database, etc)

---
+++ core-ui/scripts/setup-runConfiguration-start.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import { exec } from 'child_process';
+
+console.log('Starting Next.js development server...');
+
+const port = process.env.NEXTJS_WEB_PORT;
+
+if (!port) {
+  console.error('NEXTJS_WEB_PORT is not defined in the .env file');
+  process.exit(1);
+}
+
+const command = `npx -p node@21.1.0 next dev -p ${port}`;
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+console.log('The NextJS app is running on port:', port);
