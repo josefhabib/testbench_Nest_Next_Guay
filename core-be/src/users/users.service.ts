05bcf856ebbd3a0615829e6a9959eae015fb5165
Josef Habib <josef.habib@kcl.ac.uk>
2025-01-22 01:45:17 +0100

    (!) feat: implement createUser method in UsersService to persist new users

--- core-be/src/users/users.service.ts
+++ core-be/src/users/users.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaUsersDbService } from 'src/prisma_users-db/prisma_users-db.service';
+import { User } from '@prisma/client'; // ! Prisma generated types from schema.prisma file 
 
 @Injectable()
 export class UsersService {
 
   constructor(private readonly PrismaUsersDbService: PrismaUsersDbService){}
   
-  createUser(ctx: CreateUserDto) {
-    // TODO: Implement createUser() service method
-    return 'Stub: This action will add a new user';
+  async createUser(ctx: CreateUserDto) : Promise<User> { // NB: label asyunc (access db) an add return type  
+
+    // persist user (& return promise):
+    return this.PrismaUsersDbService.user.create({
+      data: {
+        ...ctx,
+      }
+    });
+
   }
 }
