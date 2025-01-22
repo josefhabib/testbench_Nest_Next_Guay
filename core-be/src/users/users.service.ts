f0088c5bb92ebbda0d8904a7d8514abe6e152b5d
Josef Habib <josef.habib@kcl.ac.uk>
2025-01-22 02:12:56 +0100

    feat: handle error: duplicate email

--- core-be/src/users/users.service.ts
+++ core-be/src/users/users.service.ts
@@ -1,5 +1,5 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaUsersDbService } from 'src/prisma_users-db/prisma_users-db.service';
 import { User } from '@prisma/client'; // ! Prisma generated types from schema.prisma file 
@@ -12,12 +12,19 @@ export class UsersService {
   async createUser(ctx: CreateUserDto) : Promise<User> { // NB: label asyunc (access db) an add return type  
 
     // persist user (& return promise):
-    return this.PrismaUsersDbService.user.create({
-      data: {
-        ...ctx,
-        password: await bcrypt.hash(ctx.password, 10)
+    try {
+      return await this.PrismaUsersDbService.user.create({
+        data: {
+          ...ctx,
+          password: await bcrypt.hash(ctx.password, 10)
+        }
+      });
+    } catch (error) {
+      if (error.code === 'P2002') { // Prisma unique constraint violation error code
+        throw new UnprocessableEntityException('A user with this email already exists.');
       }
-    });
+      throw error;
+    }
 
   }
 }
