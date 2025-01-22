28569382a495a24b8c1b8c08b8e97916d90533f8
Josef Habib <josef.habib@kcl.ac.uk>
2025-01-22 18:03:42 +0100

    fix/kludge: Configure NestJS application to also use .env

--- core-be/src/main.ts
+++ core-be/src/main.ts
@@ -4,9 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
-// Load environment variables from the new .env location
-dotenv.config({ path: '../.env' });
+// Load environment variables from the specified .env file
+const envFilePath = process.env.ENV_FILE_PATH || path.resolve(__dirname, '../../.env');
+dotenv.config({ path: envFilePath });
+// // -- NB - 
+// //         Although the NODE_ENV environment variable is set in the package.json scripts file (e.g. "build:prod": "dotenv -e ../../.env -- nest build") 
+// //         they must nevertheless also be included here: 
+// //         This is because environment variables are not being passed to the compiled code in the dist directory 
+// //         When you run node dist/main, it does not have access to the environment variables set during the build process.
+
 
 // TODO: Add logging
 // TODO: Add error handling (currently, NestJS will only return a 500 error (if an exception is thrown); or a 400 error (if a validation checks fail))
@@ -15,6 +23,7 @@ dotenv.config({ path: '../.env' });
 // TODO: ?Refactor: Move all the configuration to a separate file (e.g. config.ts)
 // TODO: ?Refactor: Move all the logging to a separate file (e.g. logger.ts)
 // TODO: ?Refactor: Move all the error handling to a separate file (e.g. error.ts)
+    
 
 
 async function bootstrap() {
@@ -32,7 +41,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   // - Start the app (Use configService to get port from .env)
-  await app.listen(process.env.NESTJS_CORE_PORT || 3000);
+  await app.listen(process.env.NESTJS_CORE_PORT || 3000); //TODO: Conflicing Mechanisms: We explicitly load the .env files in package.json AND we use the config module (that looks for a local .env file) - consolidate!!!
   //console.log('Listening on port: ', configService.getOrThrow('NESTJS_CORE_PORT'));
 }
 bootstrap();
