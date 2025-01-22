175a81f36c0bc50642a785eea01f5b3e7968ceb4
Josef Habib <josef.habib@kcl.ac.uk>
2025-01-22 01:18:10 +0100

    feat: inject PrismaUsersDbService into UsersService constructor

--- core-be/src/users/users.service.ts
+++ core-be/src/users/users.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaUsersDbService } from 'src/prisma_users-db/prisma_users-db.service';
 
 @Injectable()
 export class UsersService {
+
+  constructor(private readonly PrismaUsersDbService: PrismaUsersDbService){}
+  
   createUser(ctx: CreateUserDto) {
     // TODO: Implement createUser() service method
     return 'Stub: This action will add a new user';
