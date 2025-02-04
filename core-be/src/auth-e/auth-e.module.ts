0719429cd449c61ecd75b523bfef519d74ef6ea7
Josef Habib <josef.habib@kcl.ac.uk>
2025-02-04 20:33:22 +0000

    feat: integrate JwtModule with async configuration for JWT secret and expiration

--- core-be/src/auth-e/auth-e.module.ts
+++ core-be/src/auth-e/auth-e.module.ts
@@ -3,10 +3,24 @@ import { AuthEController } from './auth-e.controller';
 import { AuthEService } from './auth-e.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        secret: ConfigService.getOrThrow('NESTJS_CORE_JWT_SECRET'),
+        signOptions: {
+          expiresIn: ConfigService.getOrThrow('NESTJS_CORE_JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthEController],
-  providers: [AuthEService, LocalStrategy]
+  providers: [AuthEService, LocalStrategy],
 })
 export class AuthEModule {}
