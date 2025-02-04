dc380f11c86b83dc35d42bede094ae8d57ad215f
Josef Habib <josef.habib@kcl.ac.uk>
2025-02-04 11:01:05 +0000

    feat: extract user obj from enriched request object
    Context:
    
      A pre-requisite for creating a JWT token is to make available the necessary information:
    
      - user object
        This will contain data like the user's id, email, and other relevant information (e.g. in future we may add user-group for authoriztion).
      - JWT secret
        This is a secret key that is used to sign the JWT token. It is used to verify the authenticity of the token.
      - JWT expiry time
        This is the time after which the JWT token will expire (this is the time after which the token will no longer be valid - unless it is refreshed).
      - etc.
    
    Scope:
      In this commit we extract the user object (req.user).
      This has previously been added to the request object ("request enrichment") by the LocalAuthGuard guard (LocalAuthGuard class; validate() method)
      The password field has been redacted for security reasons (also in the validate() method).

--- core-be/src/auth-e/auth-e.controller.ts
+++ core-be/src/auth-e/auth-e.controller.ts
@@ -5,8 +5,28 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 export class AuthEController {
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req) {
-    // console.log('IN LOGIN: req.user is: ', req.user);
-    return req.user;
+  login(@Request() request) {
+    // --- Generate the JWT token here ---
+    // 1. Get the user from the request object
+    //
+    //    Notes: req.user
+    //    - This field on the request object has been added to the request object by the LocalAuthGuard guard (LocalAuthGuard class; validate() method)
+    //    - The password field has been redacted for security reasons
+    //    - This enrichment of the request object is important it allows subsequently used methods to access the user object (like JWT token generation)
+    //
+    // TODO: Add usergroup to database and include it in the JWT token
+    // TODO: Add error handling
+
+    const currentUser = request.user;
+
+    // 2. Set secret key & expiry time environment variables
+    // 3. Generate the JWT token using the sign() method
+    // 4. Return the JWT token in the response (Note: The client will store the token in the cookie)
+    //  Important settings:
+    //  - The cookie needs to be secure and httpOnly
+    //  - The cookie needs to have a sameSite attribute set to ??? (strict or lax)
+    //  - passthrough=true in the cookie settings (to ensure that the request is processed by the application and accepted by the browser)
+
+    // return request.user;
   }
 }
