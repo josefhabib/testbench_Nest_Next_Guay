4027bfa4f2309429757adfbf3b9b11a3ca3c08c2
Josef Habib <josef.habib@kcl.ac.uk>
2025-02-10 13:18:38 +0100

    refactor: comment out logging implementation in JwtAuthGuard and LocalAuthGuard (for diagnosis/debugging only)

--- core-be/src/auth-e/guards/jwt-auth.guard.ts
+++ core-be/src/auth-e/guards/jwt-auth.guard.ts
@@ -1,29 +1,31 @@
-// import { Injectable } from "@nestjs/common";
-// import { AuthGuard } from "@nestjs/passport";
-
-// @Injectable()
-// export class JwtAuthGuard extends AuthGuard('my-jwt-strategy') {}
-
-import { Injectable, ExecutionContext, UnauthorizedException, Logger } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('my-jwt-strategy') {
-  private readonly logger = new Logger(JwtAuthGuard.name);
+export class JwtAuthGuard extends AuthGuard('my-jwt-strategy') {}
 
-  canActivate(context: ExecutionContext) {
-    this.logger.log('JwtAuthGuard canActivate called');
-    const request = context.switchToHttp().getRequest();
-    this.logger.log(`Request cookies: ${JSON.stringify(request.cookies)}`);
-    return super.canActivate(context);
-  }
-
-  handleRequest(err, user, info) {
-    if (err || !user) {
-      this.logger.error('Unauthorized access', err || info);
-      throw err || new UnauthorizedException();
-    }
-    this.logger.log('User authenticated successfully');
-    return user;
-  }
-}
+// -- Logging version of the JwtAuthGuard --
+//
+// import { Injectable, ExecutionContext, UnauthorizedException, Logger } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+// 
+// @Injectable()
+// export class JwtAuthGuard extends AuthGuard('my-jwt-strategy') {
+//   private readonly logger = new Logger(JwtAuthGuard.name);
+//
+//   canActivate(context: ExecutionContext) {
+//     this.logger.log('NestJS Logger AuthGuard: JwtAuthGuard canActivate called');
+//     const request = context.switchToHttp().getRequest();
+//     this.logger.log(`NestJS Logger AuthGuard: Request cookies: ${JSON.stringify(request.cookies)}`);
+//     return super.canActivate(context);
+//   }
+//
+//   handleRequest(err, user, info) {
+//     if (err || !user) {
+//       this.logger.error('NestJS Logger AuthGuard: Unauthorized access', err || info);
+//       throw err || new UnauthorizedException();
+//     }
+//     this.logger.log('NestJS Logger AuthGuard: User authenticated successfully');
+//     return user;
+//   }
+// }
--- core-be/src/auth-e/guards/local-auth.guard.ts
+++ core-be/src/auth-e/guards/local-auth.guard.ts
@@ -4,3 +4,29 @@ import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('my-local-strategy') {}
+
+// -- Logging version of the LocalAuthGuard --
+//
+// import { Injectable, ExecutionContext, UnauthorizedException, Logger } from "@nestjs/common";
+// import { AuthGuard } from "@nestjs/passport";
+//
+// @Injectable()
+// export class LocalAuthGuard extends AuthGuard('my-local-strategy') {
+//   private readonly logger = new Logger(LocalAuthGuard.name);
+//
+//   canActivate(context: ExecutionContext) {
+//     this.logger.log('NestJS Logger AuthGuard: LocalAuthGuard canActivate called');
+//     const request = context.switchToHttp().getRequest();
+//     this.logger.log(`NestJS Logger AuthGuard: Request cookies: ${JSON.stringify(request.cookies)}`);
+//     return super.canActivate(context);
+//   }
+//
+//   handleRequest(err, user, info) {
+//     if (err || !user) {
+//       this.logger.error('NestJS Logger AuthGuard: Unauthorized access', err || info);
+//       throw err || new UnauthorizedException();
+//     }
+//     this.logger.log('NestJS Logger AuthGuard: User authenticated successfully');
+//     return user;
+//   }
+// }
