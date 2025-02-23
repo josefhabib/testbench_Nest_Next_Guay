26531f68084e73f8729d4113d0601bcf313f5e91
Josef Habib <josef.habib@kcl.ac.uk>
2025-02-23 22:45:53 +0100

    feat: Add Next.js middleware web app access control (incl re-routing) and authentication handling

--- core-ui/middleware.ts
+++ core-ui/middleware.ts
@@ -1,21 +1,57 @@
-// Next.js middleware runs before a request is processed by 
-// the Next.js server or after the response has been generated 
-// but before it is sent to the client. Middleware can be used 
-// to handle tasks such as authentication, logging, and request 
-// modification.
+/**
+ * Middleware function to handle access control in a Next.js application.
+ * 
+ * NextJS Middleware runs before a request is processed by the NextJS server 
+ *  and/or after the response has been generated but before it is sent to the 
+ *  client. It is commonly used for tasks such as authentication (here), logging,
+ *  and request modification.
+ * 
+ * Middleware functions are executed by the NextJS framework and therefore must 
+ *  be implemeneted in line with the expectations of the NextJS framework: 
+ *  - It must be stored in a project root level file called middleware.ts (@/middleware.ts)
+ *  - It must export a function called `middleware` that takes two arguments:
+ *    - `req`: The incoming request object
+ *    - `res`: The outgoing response object
+ *  - It must return a response object or undefined
+ *  - It can be configured to apply to specific routes or route patterns
+ * 
+ * Auth Middleware:
+ *  Auth middleware is applied by default to all requests. Exceptions can/must 
+ *   explicitly be defined. Here, 2 exceptions are specified:
+ *   - URL: pages starting with a certain URL pattern (publicRoutes)
+ *   - Types: Static assets (images, favicons, etc.) are not subject to the
+ *     auth middleware (config)
+ * 
+ * Functionality:
+ *   - When a request is made to the NextJS server, the middleware checks if the
+ *     exclusion exceptions apply. 
+ *      -> Is the route a public route?
+ *      -> Is the route a static asset?
+ *        - If yes, the request is processed as normal.
+ *        - If not, the middleware checks if the user is authenticated (and later authorized)
+ *     -> Is the auth cookie set on the request?
+ *        - If yes, the request is processed as normal.
+ *       - If not, the middleware redirects the user to the login screen. Once the user has 
+ *          submitted credentials (attempts to set the auth cookie `be-core-auth`), the 
+ *          middleware will automatically return the user to the page they were trying 
+ *          to access and repeat the previous attempt.
+ * 
+ * Known limitations: 
+ *  - The middleware does not handle the case where the user is authenticated 
+ *    but not authorized.
+ * 
+ * @param {NextRequest} req - The incoming request object.
+ * @param {NextResponse} res - The outgoing response object.
+ * 
+ * @returns {Response | undefined} - A redirect response to the login page if access is restricted, otherwise undefined.
+ */
 
 import { NextRequest, NextResponse } from "next/server";
-
-
 
 export function middleware(req: NextRequest, res: NextResponse){
   
   // NextJS Access Control
-  // Check if the auth cookie has been set on the current request; 
-  // -> If yes, get the value
-  // -> If not, impose access restrictions 
-  //    I.e. check if you are attempting to access an access-restricted route, 
-  //    and if you are, redirecting the user to the login page
+  // Check 
 
   const publicRoutes = [
     "/auth/login", 
@@ -30,21 +66,14 @@ export function middleware(req: NextRequest, res: NextResponse){
     authCookieValue = authCookie.value;
   } 
   else {
-    // 
-    // If the request does not have the auth cookie set... 
-    // ... check if you are attempting to access an authentication restricted route... 
-    // ... and if you are then redirect the user to the login page
-    // 
     if (!authCookieValue && !publicRoutes.some(route => req.nextUrl.pathname.startsWith(route))) {
-      // if you do NOT have an auth cookie AND the route is NOT public, redirect to the login page
       return Response.redirect(new URL("/auth/login", req.url));
     }
   }
 }
 
 export const config = {
-  // The middleware will read this config file and not apply the middleware to 
-  //  static assets (like favicon, other images) based on the supplied regex
+  // The middleware will read this config file and not apply the middleware to static assets (like favicon, other images) based on the supplied regex
   matcher: [
     "/((?!api|_next/static|_next/image|.*\\.png$).*)"
   ]
